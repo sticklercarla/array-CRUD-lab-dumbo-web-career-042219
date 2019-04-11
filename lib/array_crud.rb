d733e9398986eb53835a13f2168ece6d3c7bc6d7
Carla Stickler <carlastickler@gmail.com>
2019-04-11 15:19:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  array = []
+  []
 end
 
 def create_an_array
-  
+  ["chocolate", "vanilla", "mint", "butter pecan"]
 end
 
 def add_element_to_end_of_array(array, element)
